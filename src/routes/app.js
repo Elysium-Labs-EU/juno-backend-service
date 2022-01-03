9d210082e1f6b4d17957a3871bc71cd6483ddd61
Robbert <robberttg@protonmail.com>
2022-01-03 20:01:32 +0100

    Update app.js

--- src/routes/app.js
+++ src/routes/app.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, sentry-trace'
 	);
 	res.setHeader(
 		'Access-Control-Allow-Methods',
