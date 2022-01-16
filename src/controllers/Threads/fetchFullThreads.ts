7f9857066662f4a0891c58fe13334722b09e8acf
Robbert <robberttg@protonmail.com>
2022-01-16 15:08:33 +0100

    Change logic to 1 endpoint

--- src/controllers/Threads/fetchFullThreads.ts
+++ src/controllers/Threads/fetchFullThreads.ts
@@ -2,60 +2,79 @@ import { google } from 'googleapis';
 import { authenticated } from '../../google/index';
 import { USER } from '../../constants/globalConstants';
 
-const getThreads = (auth, req) =>
-	new Promise((resolve, reject) => {
-		const gmail = google.gmail({ version: 'v1', auth });
-		const { query } = req;
+async function asyncForEach(array, callback) {
+	for (let index = 0; index < array.length; index += 1) {
+		await callback(array[index], index, array);
+	}
+}
 
-		function listThreads() {
-			const requestBody: any = {
+const getFullThreads = async (auth, req) => {
+	const gmail = google.gmail({ version: 'v1', auth });
+
+	async function singleThread(thread) {
+		const { id } = thread;
+		try {
+			const response = await gmail.users.threads.get({
 				userId: USER,
+				id,
+				format: 'full',
+			});
+			if (response && response.data) return response.data;
+			new Error('Thread not found...');
+		} catch (err) {
+			new Error(`Threads returned an error: ${err}`);
+		}
+	}
+
+	async function listThreads() {
+		const requestBody: any = {
+			userId: USER,
+		};
+		requestBody.maxResults =
+			typeof Number(req.query.maxResults) !== 'number'
+				? 20
+				: Number(req.query.maxResults);
+		if (req.query.labelIds && req.query.labelIds !== 'undefined') {
+			requestBody.labelIds = req.query.labelIds;
+		}
+		if (req.query.pageToken) {
+			requestBody.pageToken = req.query.pageToken;
+		}
+		if (req.query.q) {
+			requestBody.q = req.query.q;
+		}
+
+		const response = await gmail.users.threads.list(requestBody);
+		if (response && response.data) {
+			const hydrateMetaList = async () => {
+				const results: any = [];
+				const threads = response.data.threads;
+				if (threads)
+					await asyncForEach(threads, async (thread) => {
+						const threadDetail = await singleThread(thread);
+						results.push(threadDetail);
+					});
+				return results;
 			};
-			requestBody.maxResults =
-				typeof Number(req.query.maxResults) !== 'number'
-					? 20
-					: Number(req.query.maxResults);
-			if (req.query.labelIds && req.query.labelIds !== 'undefined') {
-				requestBody.labelIds = req.query.labelIds;
-			}
-			if (req.query.pageToken) {
-				requestBody.pageToken = req.query.pageToken;
-			}
-			if (req.query.q) {
-				requestBody.q = req.query.q;
-			}
-
-			return new Promise((resolve, reject) => {
-				gmail.users.threads.list(requestBody, (err, res) => {
-					if (err) {
-						reject(new Error(`Threads returned an error: ${err}`));
-					}
-					if (res && res.data) {
-						resolve(res.data);
-					} else {
-						reject(new Error('No threads found...'));
-					}
-				});
-			});
+			return hydrateMetaList();
 		}
-		if (query) {
-			const threads = listThreads();
-			resolve(threads);
-		}
-		reject(new Error('No threads found...'));
-	});
+	}
+	try {
+		const threads = await listThreads();
+		if (threads) return threads;
+		return new Error('No threads found...');
+	} catch (err) {
+		new Error(`Threads returned an error: ${err}`);
+	}
+};
 
-export const fetchFullThreads = (req, res) => {
-	authenticated
-		.then((auth) => {
-			getThreads(auth, req).then((response) => {
-				res.status(200).json(response);
-			});
-		})
-		.catch((err) => {
-			res.status(404).json(err);
-		})
-		.catch((err) => {
-			res.status(401).json(err);
-		});
+export const fetchFullThreads = async (req, res) => {
+	try {
+		const auth = await authenticated;
+		const response = await getFullThreads(auth, req);
+		return res.status(200).json(response);
+	} catch (err) {
+		res.status(404).json(err);
+		res.status(401).json(err);
+	}
 };
