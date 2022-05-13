834bf50fd329a8e4c766675f761eac51a424a282
Robbert <robberttg@protonmail.com>
2022-05-13 08:37:55 +0200

    Update logoutUser.ts

--- src/controllers/Users/logoutUser.ts
+++ src/controllers/Users/logoutUser.ts
@@ -1,7 +1,6 @@
 export const logoutUser = async (req, res) => {
   try {
     if (req.headers.authorization) {
-      console.log(req.headers.authorization)
       req.session.destroy(function (err) {
         if (err) {
           console.log(err)
