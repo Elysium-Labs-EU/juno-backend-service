9db768f1d5c3c3d158116a2bb34aac4604ecfe7f
Robbert <robberttg@protonmail.com>
2022-08-19 11:31:14 +0200

    Update app.ts

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -42,6 +42,7 @@ app.use((req, res, next) => {
     'No Frontend environment variable found.'
   )
   res.setHeader('credentials', 'include')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL)
   res.setHeader(
     'Access-Control-Allow-Headers',
@@ -51,7 +52,6 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, DELETE, PATCH, OPTIONS'
   )
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
   next()
 })
 
