523c1944fd9b98d5e301c0cd49fd5d20cec4b4a5
Robbert <robberttg@protonmail.com>
2022-10-13 22:52:31 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT || 5001
 import http from 'http'
-import application from './routes/app'
+import application from './src/routes/app'
 
 const server = http.createServer(application)
 server.listen(PORT)
