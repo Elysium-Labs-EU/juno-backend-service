ce5c534d6b6a6f7f421b4b30f81f3120f8ab4ecf
Robbert <robberttg@protonmail.com>
2023-01-27 16:54:55 +0100

    Add JSDoc

--- src/utils/onlyLegalLabelObjects.ts
+++ src/utils/onlyLegalLabelObjects.ts
@@ -5,6 +5,20 @@ interface IOnlyLegalLabels {
   storageLabels: Array<LabelIdName>
 }
 
+/**
+ * @function
+ * @param {IOnlyLegalLabels} param0 - An object with two properties:
+ * labelNames: an array of strings representing the names of labels
+ * storageLabels: an array of LabelIdName objects
+ * @return {IOnlyLegalLabels['storageLabels']} - an array of LabelIdName objects that match the labelNames
+ * @typedef {Object} LabelIdName
+ * @property {string} id
+ * @property {string} name
+ * @typedef {Object} IOnlyLegalLabels
+ * @property {Array<string>} labelNames
+ * @property {Array<LabelIdName>} storageLabels
+ */
+
 const onlyLegalLabels = ({ labelNames, storageLabels }: IOnlyLegalLabels) => {
   const idMapStorageLabels = storageLabels.map((label) => label.id)
 
